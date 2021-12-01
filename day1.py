f34498710b2a9df15de9dbcc82f783c6ab5ea148
pardeema <pardeema@users.noreply.github.com>
2021-12-01 10:29:06 -0700

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+FILE1 = 'input1_1.txt'
+
+pt1 = 0
+pt2 = 0 
+
+with open(FILE1, 'r') as f:
+    lines = f.readlines()
+
+for i in range(len(lines)-1):
+    if int(lines[i+1]) > int(lines[i]):
+        pt1+=1
+for i in range(len(lines)-3):
+    sum_A = int(lines[i]) + int(lines[i+1]) + int(lines[i+2])
+    sum_B = int(lines[i+1]) + int(lines[i+2]) + int(lines[i+3])
+    if sum_B > sum_A:
+        pt2+=1
+
+print(pt1)
+print(pt2)
